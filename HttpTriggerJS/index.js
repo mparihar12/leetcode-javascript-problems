61fd304dee12b8313f604e035baaf7f40fc8ba6d
mparihar12 <monikaparihar@hotmail.com>
2018-10-13 16:49:55 -0700

    azure app

--- HttpTriggerJS/index.js
+++ HttpTriggerJS/index.js
@@ -1,10 +1,12 @@
-module.exports = async function (context, req) {
+export default async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
+    
+    
+        if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Welcome " + (req.query.name || req.body.name)
+            body: "Welcomesss " + (req.query.name || req.body.name)
         };
     }
     else {
@@ -13,4 +15,9 @@ module.exports = async function (context, req) {
             body: "Please pass a name on the query string or in the request body"
         };
     }
+
+    context.res = {
+        status: 400,
+        body: req.body
+    };
 };
